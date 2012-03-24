75746e41952a44d16f938bd9b67d1a725896f2d6
jroca72 <jroca72@gmail.com>
2012-03-24 23:51:27 +0100

    frontend primeros pasos

--- src/Siga21/FrontendBundle/Controller/PedidoController.php
+++ src/Siga21/FrontendBundle/Controller/PedidoController.php
@@ -3,13 +3,25 @@
 namespace Siga21\FrontendBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Siga21\BackendBundle\Entity\tienda;
+use Siga21\BackendBundle\Entity\articulo;
 
 class PedidoController extends Controller
 {
     
     public function portadaAction()
     {
-        return $this->render('FrontendBundle::frontend.html.twig');
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $entities = $em->getRepository('BackendBundle:tienda')->findById('4');
+
+        $em1 = $this->getDoctrine()->getEntityManager();
+        $entities2 = $em1->getRepository('BackendBundle:articulo')->findById('3');
+
+        return $this->render('FrontendBundle::frontend.html.twig', array(
+            'entities' => $entities, 'entities2' => $entities2
+        ));
+
+
     }
 }
